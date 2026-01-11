863959884459509f11583d7f32706fe6efe2b99c
Aditya <adithya18062000@gmail.com>
2026-01-12 02:13:07 +0530

    fix: templates dir path for web inspector

--- internal/ui/web/web.go
+++ internal/ui/web/web.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"html/template"
 	"net/http"
+	"path/filepath"
 )
 
 type WebServer struct {
@@ -12,8 +13,11 @@ type WebServer struct {
 }
 
 func (ws *WebServer) StartServer() error {
+	templatesDir := "internal/ui/web/templates"
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("internal/web/templates/index.html")
+		templatePath := filepath.Join(templatesDir, "index.html")
+		tmpl, err := template.ParseFiles(templatePath)
 		if err != nil {
 			// fmt.Println(err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
